4c6d35ffc038d97ab954f1fc6b2a2b308da4172e
Hugo Häggmark <hugo.haggmark@gmail.com>
2015-03-16 17:51:51 +0100

    fixed inputs

--- static/js/erekonfa.js
+++ static/js/erekonfa.js
@@ -1,20 +1,40 @@
+var getQueryVariable = function(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return pair[1];
+        }
+    }
+
+    return (false);
+};
+countdown.setLabels(
+    ' millissekund| sekund| minut| timme| dag| vecka| månad| år| decenium| sekel| millenium',
+    ' millissekunder| sekunder| minuter| timmar| dagar| veckor| månader| år| décadas| séculos| milênios',
+    ' och ',
+    ', ',
+    'kvar');
+
 $(document).ready(function() {
-    countdown.setLabels(
-        ' millissekund| sekund| minut| timme| dag| vecka| månad| år| decenium| sekel| millenium',
-        ' millissekunder| sekunder| minuter| timmar| dagar| veckor| månader| år| décadas| séculos| milênios',
-        ' och ',
-        ', ',
-        'kvar');
-    var name = "Engelberg";
-    var startDate = moment("2015-03-20 00:00:01");
+    var name = getQueryVariable("namn") || "Engelberg";
+    var start = getQueryVariable("start") || "2015-03-20 00:00:01";
+    var slut = getQueryVariable("slut") || "2015-03-23 19:00:00";
+    var startDate = moment(start);
+    var stopDate = moment(slut);
     var today = moment(new Date());
     var timerId = countdown(startDate, function(ts) {
         $(".tid").text(ts + " kvar till " + name + "!");
     });
+
     if (today.isBefore(startDate)) {
         $(".konfa").text("Nope!");
+    } else if (today.isAfter(stopDate)) {
+        $(".konfa").text("Nope! den är över :(");
+        $(".tid").hide();
     } else {
-        window.clearInterval(timerId);
         $(".konfa").text("Jepp!");
+        $(".tid").hide();
     }
 });
